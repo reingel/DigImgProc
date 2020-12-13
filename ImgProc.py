02cfc42cd11ca63f605809c4b30a003051ee3562
Soonkyu Jeong <reingel@gmail.com>
2020-12-13 15:26:48 +0900

    Final

--- ImgProc.py
+++ ImgProc.py
@@ -1,12 +1,9 @@
 import numpy as np
 import numpy.random as rd
-import matplotlib.pyplot as plt
 from PIL import Image
 
-
-# constants
-nBit = np.uint8(8) # number of bits to represent a pixel
-L = np.uint(2**nBit) # number of gray-scale levels
+nBit = np.int(8) # number of bits for gray levels
+L = np.int(2**nBit) # number of gray levels
 IMIN = np.uint8(0) # black
 IMAX = np.uint8(L - 1) # white
 
@@ -18,11 +15,20 @@ class ImgProcLib:
     def load(self, filepath):
         # load an image
         img = Image.open(filepath)
-        # get info
+
+        # get data & info
+        data = np.array(img)
         width, height = img.size
         mode = 'B&W' if img.mode == 'L' else img.mode
+
         # convert into a numpy array
-        f = np.array(img, dtype=np.uint8)
+        if mode == 'B&W':
+            f = np.array(data, dtype=np.uint8)
+        elif mode == 'RGB' or mode == 'RGBA':
+            f = np.array(np.mean(data[:,:,:3], axis=2), dtype=np.uint8)
+        else:
+            print('Unknown mode!')
+            return
 
         if self.show: img.show(title='original image')
         if self.verbose: print(f'Image loaded: {filepath}, ({width} x {height}), ({mode})')
@@ -76,33 +82,197 @@ class ImgProcLib:
         return g
 
     def histogram(self, f):
-        # return histogram of input image
-        g = f.flatten()
-        N = len(g)
-
-        # array for histogram
-        h = np.zeros(L, dtype=np.uint16)
-        # accumulate for every pixel
-        for s in range(N):
-            level = g[s]
-            h[level] += 1
-        
+        h = np.zeros(L, dtype=np.int)
+        for s in f.flatten():
+            h[s] += 1
         return h
     
     def prob_dist(self, f):
         N = len(f.flatten())
         h = self.histogram(f)
-        h = np.array(h)
         return h / N
-    
-    def threshold_otsu(self, f):
+
+    def threshold_otsu2(self, f):
+        i = range(L)
         p = self.prob_dist(f)
-        w = np.cumsum(p) # class probability vector
-        w[(w == 0) + (w == 1)] = 1e-9
-        u = np.cumsum(range(L)*p) # class mean vector
-        uT = u[-1] # total mean
-        sB2 = (uT*w - u)**2 / (w * (1 - w)) # between-class variance vector
-        k_star = np.argmax(sB2)
+        w_k = np.cumsum(p) # class probability vector
+        mu_k = np.cumsum(i * p) # class mean vector
+        mu_T = mu_k[-1] # total mean
+        var_B = np.zeros_like(w_k)
+        for k in range(1,L):
+            if 0 < w_k[k] < 1:
+                var_B[k] = (mu_T * w_k[k] - mu_k[k])**2 / (w_k[k] * (1 - w_k[k]))
+        var_T = np.sum((i - mu_T)**2 * p)
+        # get optimal intensity
+        k_opt = np.argmax(var_B)
+        # extract the statistical information
+        var_B_opt = var_B[k_opt]
+        eta_opt = var_B_opt / var_T
+        w_0 = w_k[k_opt]
+        w_1 = 1 - w_0
+        mu_0 = mu_k[k_opt] / w_0
+        mu_1 = (mu_T - mu_0 * w_0) / (1 - w_0)
+        # generate binary image
         g = np.zeros_like(f)
-        g[f > k_star] = IMAX
-        return g, p, sB2, k_star
+        g[f >= k_opt] = IMAX
+
+        if self.verbose:
+            print(f'{mu_T = :.1f}, sigma_T = {np.sqrt(var_T):.1f}')
+            print(f'{k_opt = }, {eta_opt = :.3f}')
+            print(f'{w_0 = :.3f}, {w_1 = :.3f}')
+            print(f'{mu_0 = :.1f}, {mu_1 = :.1f}')
+        
+        return g, p, w_k, mu_k, mu_T, var_B, var_T, (k_opt), var_B_opt, eta_opt, (w_0, w_1), (mu_0, mu_1)
+
+    def threshold_otsu3(self, f):
+        i = range(L)
+        p = self.prob_dist(f)
+        w_k = np.cumsum(p) # class probability vector
+        mu_k = np.cumsum(i * p) # class mean vector
+        mu_T = mu_k[-1] # total mean
+        var_B = np.zeros((L,L))
+        for k1 in range(1,L-1): # k1 = 0, 1, ..., L - 2
+            for k2 in range(k1+1, L): # k2 = k1 + 1, k1 + 2, ..., L - 1
+                w_k1 = w_k[k1]
+                w_k2 = w_k[k2]
+                mu_k1 = mu_k[k1]
+                mu_k2 = mu_k[k2]
+                w = np.array([w_k1, w_k2 - w_k1, 1 - w_k2])
+                if np.min(w**2) > 0:
+                        mu = np.array([mu_k1, mu_k2 - mu_k1, mu_T - mu_k2]) / w
+                        var_B[k1][k2] = np.sum(w * (mu - mu_T)**2)
+        var_T = np.sum((i - mu_T)**2 * p)
+        # get optimal intensity (axis=None means the index is into the flattened array)
+        k1_opt, k2_opt = np.unravel_index(np.argmax(var_B, axis=None), var_B.shape)
+        # extract the statistical information
+        var_B_opt = var_B[k1_opt][k2_opt]
+        eta_opt = var_B_opt / var_T
+        w_0 = w_k[k1_opt]
+        w_1 = w_k[k2_opt] - w_0
+        w_2 = 1 - w_k[k2_opt]
+        mu_0 = mu_k[k1_opt] / w_0
+        mu_1 = (mu_k[k2_opt] - mu_k[k1_opt]) / w_1
+        mu_2 = (mu_T - mu_k[k2_opt]) / w_2
+        # generate binary image
+        g = np.zeros_like(f)
+        g[(k1_opt <= f) & (f < k2_opt)] = IMAX / 2
+        g[k2_opt <= f] = IMAX
+
+        if self.verbose:
+            print(f'{mu_T = :.1f}, sigma_T = {np.sqrt(var_T):.1f}')
+            print(f'{k1_opt = }, {k2_opt = }, {eta_opt = :.3f}')
+            print(f'{w_0 = :.3f}, {w_1 = :.3f}, {w_2 = :.3f}')
+            print(f'{mu_0 = :.1f}, {mu_1 = :.1f}, {mu_2 = :.1f}')
+        
+        return g, p, w_k, mu_k, mu_T, var_B, var_T, (k1_opt, k2_opt), var_B_opt, eta_opt, (w_0, w_1, w_2), (mu_0, mu_1, mu_2)
+
+    def threshold_moving_average(self, f, n=5, b=0.5):
+        height, width = f.shape
+        z = np.array(f.flatten(), dtype=np.int)
+        N = len(z)
+        m = np.zeros_like(z)
+        for k in range(N-1): # k = 0, 1, ..., N-2
+            zkp1 = z[k+1]
+            zkmn = z[k-n] if k >= n else 0
+            m[k+1] = m[k] + (zkp1 - zkmn) / n
+        t = np.reshape(m, f.shape)
+        g = np.zeros_like(f)
+        # local thresholding
+        for y in range(height):
+            for x in range(width):
+                Sxy, _ = self.get_pixel_rel_win(t, y, x, 5, True)
+                mxy = np.mean(Sxy)
+                g[y, x] = 1 if f[y, x] > b * mxy else 0
+
+        return g, t
+
+
+    #
+    # Report 1
+    #
+    def add_salt_and_pepper(self, f, ratio_salt=0.05, ratio_pepper=0.05):
+        # add salt-and-pepper noise
+        fn = f.copy()
+        height, width = fn.shape
+
+        # calculate no. of pixels to be noised
+        n_salt = np.int(width * height * ratio_salt)
+        n_pepper = np.int(width * height * ratio_pepper)
+        # add salt noises
+        for _ in range(n_salt):
+            y, x = rd.randint(height), rd.randint(width)
+            fn[y, x] = IMAX
+        # add pepper noises
+        for _ in range(n_pepper):
+            y, x = rd.randint(height), rd.randint(width)
+            fn[y, x] = IMIN
+
+        if self.show: self.imshow(fn, title='Image with salt-and-pepper noise')
+        if self.verbose: print(f'Added salt-and-pepper noise: no. of salt, pepper = ({n_salt}, {n_pepper})')
+
+        return fn
+
+    def median_filter(self, f, win_size=3, inc_bound=True):
+        #
+        # median filter
+        #
+        # f: input image data
+        # win_size: filter size
+        # inc_bound: filter boundary if True
+        # g: filtered image data
+        #
+        g = np.zeros_like(f)
+        height, width = f.shape
+        ofs = win_size // 2 # offset = (window size) / 2 - 0.5
+
+        for y in range(height):
+            for x in range(width):
+                # get 1D pixel data from filter window and boundary-ness
+                f_win, is_boundary = self.get_pixel_rel_win(f, y, x, ofs, flatten=True)
+                # filter boundary if inc_bound = True
+                if (not is_boundary) or (is_boundary and inc_bound):
+                    # calculate index to median pixel
+                    i_med = len(f_win) // 2
+                    # sort, extract and assign median pixel data
+                    g[y, x] = sorted(f_win)[i_med]
+        
+        if self.show: self.imshow(g, title='Filtered image')
+        if self.verbose: print(f'Image is denoised with a median filter (size = {win_size})')
+
+        return g
+
+    def adaptive_median_filter(self, f, win_max):
+        #
+        # adaptive median filter
+        #
+        # f: input image data
+        # win_max: maximum filter window size
+        # g: filtered image data
+        #
+        g = np.zeros_like(f)
+        height, width = f.shape
+        S_max = win_max
+
+        for y in range(height):
+            for x in range(width):
+                z_xy = f[y, x]
+                for S in range(3, S_max + 1, 2): # S = 3, 5, ..., S_max
+                    ofs = S // 2
+                    f_win, _ = self.get_pixel_rel_win(f, y, x, ofs, flatten=True)
+                    zs = np.array(sorted(f_win), dtype=np.int)
+                    z_min = zs[0]
+                    z_max = zs[-1]
+                    z_med = zs[len(zs) // 2]
+                    # Stage A
+                    if z_min < z_med < z_max: # successfully filtered
+                        # Stage B (check if not a noise)
+                        z = z_xy if z_min < z_xy < z_max else z_med
+                        break
+                    else: # too many noises to filter with small window
+                        z = z_med
+                g[y, x] = z
+        
+        if self.show: self.imshow(g, title='Filtered image')
+        if self.verbose: print(f'Image is denoised with an adaptive median filter (max. size = {S_max})')
+
+        return g
--- main.py
+++ main.py
@@ -1,57 +1,158 @@
 import numpy as np
 import numpy.random as rd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from ImgProc import ImgProcLib
 
-# parameters
-filepath = 'camera.bmp'
+nBit = np.int(8) # number of bits for gray levels
+L = np.int(2**nBit) # number of gray levels
+IMIN = np.uint8(0) # black
+IMAX = np.uint8(L - 1) # white
+
+# image filenames
+filenames = [
+    'Fig2.07(a).jpg',
+    'A_1.png',
+    'A_2.png',
+    'mesh.png',
+    'honeywell.png',
+    'stain_1.png',
+    'stain_2.png',
+    'script1.jpeg',
+    'script2.jpeg',
+    'Fig2.08(a).jpg',
+    'Fig2.08(b).jpg',
+    'Fig2.08(c).jpg',
+    'Fig2.19(a).jpg',
+    'Fig2.21(a).jpg',
+    'Fig2.22(a).jpg',
+    'Fig2.22(b).jpg',
+    'Fig2.22(c).jpg',
+    # 'Fig2.24.jpg',
+    'Fig5.07(a).jpg',
+    # 'Fig10.1(a).png',
+    # 'Fig10.16(a).png',
+    'Fig10.21(b).png',
+    # 'Fig10.26(a).png',
+    'Fig10.36(a).png',
+    'Fig10.36(b).png',
+    'Fig10.36(c).png',
+    'Fig10.37(a).png',
+    # 'Fig10.37(b).png',
+    'Fig10.37(c).png',
+    'Fig10.38(a).png',
+    # 'Fig10.39(a).png',
+    'Fig10.43(a).png',
+    'Fig10.45(a).png',
+    'Fig10.49(a).png',
+    'Fig10.50(a).png',
+]
 
 # create a ImgProcLib object
-ipl = ImgProcLib(show=False, verbose=True)
+ipl = ImgProcLib(show=False, verbose=False)
 
-# load image file
-f = ipl.load(filepath)
+for filename in filenames:
+    # load image file
+    f = ipl.load('images/' + filename)
+    
+    # show the original image
+    fig, ((ax1, ax2, ax3), (ax4, ax5, ax6), (ax7, ax8, ax9)) = plt.subplots(3, 3, figsize=(10,8))
 
-# calculate probability distribution
-p = ipl.prob_dist(f)
-g, p, sB2, k_star = ipl.threshold_otsu(f)
+    ax1.imshow(f, cmap='gray')
+    ax1.set_title(f'Original image: {filename}', fontweight='bold', fontsize=8)
+    ax1.set_axis_off()
 
-plt.subplot(221)
-plt.imshow(f, cmap='gray')
+    # Otsu's thresholding with two classes
+    g, p, w_k, mu_k, mu_T, var_B, var_T, k_opt, var_B_opt, eta_opt, w_opt, mu_opt = ipl.threshold_otsu2(f)
+    sigma_B = np.sqrt(var_B)
+    sigma_T = np.sqrt(var_T)
+    sigma_B_opt = np.sqrt(var_B_opt)
+    k_opt2 = k_opt
 
-plt.subplot(222)
-plt.imshow(g, cmap='gray')
+    # show the segmented image
+    ax2.imshow(g, cmap='gray')
+    ax2.set_title("Otsu's thresholding with two classes", fontweight='bold', fontsize=8)
+    ax2.set_axis_off()
 
-plt.subplot(223)
-plt.plot(p)
-plt.plot(sB2/np.max(sB2))
-plt.plot([k_star, k_star], [0, 1])
+    # show the histogram and class means
+    ax5.vlines(range(0, L, 4), 0, p[::4], 'k', linewidth=0.5)
+    # ax5.plot(w_k, 'r')
+    ax5.vlines(k_opt, 0, np.max(p), 'r')
+    ax5.vlines(mu_T, 0, np.max(p) * 2/3, 'c')
+    ax5.vlines(mu_opt, 0, np.max(p) * 1/2, 'b')
+    ax5.tick_params(axis='both', labelsize=7)
+    ax5.set_xlabel('gray-level intensity', fontsize=7)
+    ax5.set_ylabel('probability', fontsize=7)
+
+    # show the between-class variance and optimal threshold
+    ax8.plot(sigma_B, 'g', linewidth=0.5)
+    ax8.plot([k_opt, k_opt], [0, sigma_B_opt], 'r')
+    ax8.tick_params(axis='both', labelsize=7)
+    ax8.set_xlabel('gray-level intensity', fontsize=7)
+    ax8.set_ylabel('between-class std', fontsize=7)
+    
+    # show information about total data
+    ax4.text(0, 0.8, 'Total mean & std', fontweight='bold', fontsize=8)
+    ax4.text(0, 0.7, f'$\mu_T$ = {mu_T:.1f}, $\sigma_T$ = {sigma_T:.1f}', fontsize=8)
+
+    # show information about two-class thresholding
+    ax4.text(0, 0.5, 'two-class thresholding', fontweight='bold', fontsize=8)
+    ax4.text(0, 0.4, f'$k^*$ = {k_opt}', fontsize=8)
+    ax4.text(0, 0.3, f'$\sigma_B^*$ = {sigma_B_opt:.1f}, $\eta^*$ = {eta_opt:.3f}', fontsize=8)
+    ax4.text(0, 0.2, f'$\omega^*$ = ({w_opt[0]:.2f}, {w_opt[1]:.2f})', fontsize=8)
+    ax4.text(0, 0.1, f'$\mu^*$ = ({mu_opt[0]:.1f}, {mu_opt[1]:.1f})', fontsize=8)
+    ax4.set_axis_off()
+
+    # Otsu's thresholding with three classes
+    g, p, w_k, mu_k, mu_T, var_B, var_T, k_opt, var_B_opt, eta_opt, w_opt, mu_opt = ipl.threshold_otsu3(f)
+    sigma_B = np.sqrt(var_B)
+    sigma_T = np.sqrt(var_T)
+    sigma_B_opt = np.sqrt(var_B_opt)
+
+    # show the segmented image
+    ax3.imshow(g, cmap='gray')
+    ax3.set_title("Otsu's thresholding with three classes", fontweight='bold', fontsize=8)
+    ax3.set_axis_off()
+
+    # show the histogram and class means
+    ax6.vlines(range(0, L, 4), 0, p[::4], 'k', linewidth=0.5)
+    # ax6.plot(mu_k, 'r')
+    ax6.vlines(k_opt, 0, np.max(p), 'r')
+    ax6.vlines(mu_T, 0, np.max(p) * 2/3, 'c')
+    ax6.vlines(mu_opt, 0, np.max(p) * 1/2, 'b')
+    ax6.tick_params(axis='both', labelsize=7)
+    ax6.set_xlabel('gray-level intensity', fontsize=7)
+    ax6.set_ylabel('probability', fontsize=7)
+
+    # show the between-class variance and optimal thresholds
+    x, y = np.meshgrid(np.arange(L), np.arange(L))
+    ax9.contour(x, y, np.transpose(var_B), np.linspace(1, np.max(var_B), 50), linewidths=0.5)
+    ax9.plot([0, k_opt[0], k_opt[0]], [k_opt[1], k_opt[1], 0], 'r')
+    ax9.plot([0, 20], [k_opt2, k_opt2], 'r')
+    ax9.plot([k_opt2, k_opt2], [235, 255], 'r')
+    ax9.tick_params(axis='both', labelsize=7)
+    ax9.set_xlabel('optimal threshold 1', fontsize=7)
+    ax9.set_ylabel('optimal threshold 2', fontsize=7)
+    
+    # show information about three-class thresholding
+    ax7.text(0, 0.8, 'three-class thresholding', fontweight='bold', fontsize=8)
+    ax7.text(0, 0.7, f'$k^*$ = {k_opt}', fontsize=8)
+    ax7.text(0, 0.6, f'$\sigma_B^*$ = {sigma_B_opt:.1f}, $\eta^*$ = {eta_opt:.3f}', fontsize=8)
+    ax7.text(0, 0.5, f'$\omega^*$ = ({w_opt[0]:.2f}, {w_opt[1]:.2f}, {w_opt[2]:.2f})', fontsize=8)
+    ax7.text(0, 0.4, f'$\mu^*$ = ({mu_opt[0]:.1f}, {mu_opt[1]:.1f}, {mu_opt[2]:.1f})', fontsize=8)
+    ax7.set_axis_off()
+
+    # thresholding using moving averages
+    # if filename == 'Fig10.49(a).png' or filename == 'Fig10.50(a).png':
+    #     g, t = ipl.threshold_moving_average(f, n=20, b=0.8)
+    #     fig = plt.figure(figsize=(10/3,8/3*2))
+    #     ax = fig.add_subplot(211)
+    #     ax.imshow(t, cmap='gray')
+    #     ax.set_axis_off()
+    #     ax.set_title('moving average', fontweight='bold', fontsize=8)
+    #     ax = fig.add_subplot(212)
+    #     ax.imshow(g, cmap='gray')
+    #     ax.set_axis_off()
+    #     ax.set_title('thresholding', fontweight='bold', fontsize=8)
 
 plt.show()
-
-
-'''
-p = [f, fn, None] # image array
-t = [f'original image (diff={d:.2f})', f'corrupted by salt-and-pepper (diff={dn:.2f})', None] # title array
-for s in range(3,7+1,2):
-    g = ipl.median_filter(fn, win_size=s)
-    h = ipl.difference(f, g)
-    d = np.sqrt(np.mean(h**2))
-    p.append(g)
-    t.append(f'median filter (size={s}, diff={d:.2f})')
-for s in range(3,7+1,2):
-    g = ipl.adaptive_median_filter(fn, win_max=s)
-    h = ipl.difference(f, g)
-    d = np.sqrt(np.mean(h**2))
-    p.append(g)
-    t.append(f'adaptive median filter (max. size={s}, diff={d:.2f})')
-
-fig, axis = plt.subplots(nrows=3, ncols=3)
-axis = axis.flatten()
-for i in range(len(p)):
-    if np.array(p[i]).any():
-        axis[i].imshow(p[i], cmap='gray')
-        axis[i].set_title(t[i], fontsize=7)
-    axis[i].set_axis_off()
-plt.show()
-'''
